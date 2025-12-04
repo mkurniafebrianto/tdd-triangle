ceb41134f29c5e54f865b2750f1e383edfef83bd
mkurniafebrianto <mkurniafebrianto@gmail.com>
2025-12-04 12:58:42 +0700

    change exception message

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,27 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  String detectTriangle(num sideA, num sideB, num sideC) {
+    final sides = [sideA, sideB, sideC];
+    sides.sort();
+
+    for (var side in sides) {
+      if (side < 1) {
+        throw Exception();
+      }
+    }
+
+    if (sides[0] + sides[1] <= sides[2]) {
+      throw Exception('This is Inequal Triangle');
+    }
+
+    return 'This is Equal Triangle';
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
